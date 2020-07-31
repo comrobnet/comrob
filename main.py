2a5eb684783dcda2f888ae5f010f0c38eb4ab7ba
alr <comrobnet@gmail.com>
2020-07-31 12:38:25 +0200

    continued working on main loop

--- main.py
+++ main.py
@@ -5,11 +5,21 @@ This file contains the main fucntion of the Python TCP server / robot controller
 import socket
 import time
 
+from py_tcp_server.mock_swift_api import MockSwiftApi
 from py_tcp_server.tcp_server import TcpServer
-from uArm-Python-SDK.uarm.wrapper import SwiftAPI
+from uarm_python_sdk.uarm.wrapper import SwiftAPI
+
 
 def main():
     # connect to uArm
+    try:
+        swift = SwiftAPI(filters={'hwid': 'USB VID:PID=2341:0042'})
+        print("comrob: Connected to robot.")
+    except Exception:
+        # can not be done differently due to coding of swift API
+        swift = MockSwiftApi()
+        print("comrob: Connected to mock robot.")
+
     # start TCP server
     print("comrob: Starting TCP server.")
     address = "localhost"
@@ -25,6 +35,13 @@ def main():
             connection = server.connect(address, port, connect_timeout)
             "comrob: Connected."
             data = connection.recv(data_buffer_size)
+            try:
+                # call function
+                TcpServer.function_from_json(swift, data)
+            except Exception:
+                # TODO (ALR): replace with message
+                print("comrob: failed to call function")
+
 
         except socket.timeout:
             server.close()
